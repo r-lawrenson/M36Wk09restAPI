3a167f43b515cbdf231fccf650ce80c41b9fc0b2
meesha94 <95078931+meesha94@users.noreply.github.com>
2022-05-24 16:27:50 +0100

    added UD functionality from CRUD operations

--- src/resources/resourceController.js
+++ src/resources/resourceController.js
@@ -23,3 +23,90 @@ exports.listResource = async (req,res) => {
     })
 }
 }
+
+exports.updateResource = async (req, res) => {
+	try {
+	const updateResource = await Resource.updateOne(
+		{ username: req.body.username }, 
+		{$set: 
+			{
+			username: req.body.username,
+			website: req.body.website,
+            helpline: req.body.helpline,
+            keywords: req.body.keywords
+		}})
+			res.status(200).send({ updateResource })
+    } catch (error) {
+		console.log(error)
+		res.status(500).send({ error: error.message })
+	}
+
+}
+
+exports.updateResourceWeb = async (req, res) => {
+	try {
+	const updateResourceWeb = await Resource.updateOne(
+		{ website: req.body.website }, 
+		{$set: 
+			{
+			username: req.body.username,
+			website: req.body.website,
+            helpline: req.body.helpline,
+            keywords: req.body.keywords
+		}})
+			res.status(200).send({ updateResourceWeb })
+    } catch (error) {
+		console.log(error)
+		res.status(500).send({ error: error.message })
+	}
+
+}
+
+exports.updateResourceHelp = async (req, res) => {
+	try {
+	const updateResourceHelp = await Resource.updateOne(
+		{ helpline: req.body.helpline }, 
+		{$set: 
+			{
+			username: req.body.username,
+			website: req.body.website,
+            helpline: req.body.helpline,
+            keywords: req.body.keywords
+		}})
+			res.status(200).send({ updateResourceHelp })
+    } catch (error) {
+		console.log(error)
+		res.status(500).send({ error: error.message })
+	}
+
+}
+
+exports.updateResourceKeywords = async (req, res) => {
+	try {
+	const updateResourceKeywords = await Resource.updateOne(
+		{ keywords: req.body.keywords }, 
+		{$set: 
+			{
+			username: req.body.username,
+			website: req.body.website,
+            helpline: req.body.helpline,
+            keywords: req.body.keywords
+		}})
+			res.status(200).send({ updateResourceKeywords })
+    } catch (error) {
+		console.log(error)
+		res.status(500).send({ error: error.message })
+	}
+
+}
+
+exports.deleteResource = async (req, res) => {
+	try {
+		const remove = await Resource.remove(
+			{username: req.body.username})
+			res.status(200).send({ remove })
+	} catch (error) {
+		console.log(error)
+		res.status(500).send({ error: error.message })
+	}
+}
--- src/resources/resourceRoutes.js
+++ src/resources/resourceRoutes.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 
-const { addResource, listResource } = require('./resourceController')
+const { addResource, listResource, updateResource, updateResourceWeb, updateResourceHelp, updateResourceKeywords, deleteResource } = require('./resourceController')
 
 const resourceRouter = Router
 ();
@@ -8,5 +8,15 @@ const resourceRouter = Router
 resourceRouter.post('/resource', addResource)
 
 resourceRouter.get('/resource', listResource)
+
+resourceRouter.patch('/resource', updateResource)
+
+//resourceRouter.patch('/resource', updateResourceWeb) 
+
+//resourceRouterHelp.patch('/resource', updateResourceHelp)
+
+//resourceRouterKeywords.patch('/resource', updateResourceKeywords)
+
+resourceRouter.delete('/resource', deleteResource)
 
 module.exports = resourceRouter;
