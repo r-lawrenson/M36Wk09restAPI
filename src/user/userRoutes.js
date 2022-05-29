4b6ffc9efaa9ccc559f4ae51082371d9cb9a7921
r-lawrenson <roblawrenson@hotmail.co.uk>
2022-05-29 23:19:32 +0100

    moved login route to user

--- src/user/userRoutes.js
+++ src/user/userRoutes.js
@@ -1,11 +1,11 @@
 const { Router } = require('express') ;
-const { hashPass } = require('../middleware');
+const { hashPass, verifyUser } = require('../middleware');
 
 const { addUser, listUsers, updateUser, deleteUser } = require('./userController')
 
 const userRouter = Router();
 
-userRouter.get('/login', tokenCheck, login); // moved from login routes
+userRouter.get('/login', verifyUser, login); // moved from login routes
 userRouter.post('/signup', hashPass, addUser)
 userRouter.get('/user', hashPass, listUsers)
 userRouter.patch('/resetpassword', hashPass, updateUser)
