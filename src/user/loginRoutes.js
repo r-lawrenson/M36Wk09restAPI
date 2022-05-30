47258cbe31a4297d53ef8c754ab742df4e0ecb7e
r-lawrenson <roblawrenson@hotmail.co.uk>
2022-05-30 16:47:56 +0100

    removed unnecessary files

--- src/user/loginRoutes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express') ;
-const { login, tokenCheck } = require('../middleware');
-
-const loginRouter = Router();
-
-loginRouter.post('/login', tokenCheck, login)
-
-module.exports = loginRouter;
