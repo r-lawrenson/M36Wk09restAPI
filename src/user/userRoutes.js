c98c386cd564460098fbdbe58d57385f1354af4d
r-lawrenson <roblawrenson@hotmail.co.uk>
2022-05-30 16:53:26 +0100

    edited require

--- src/user/userRoutes.js
+++ src/user/userRoutes.js
@@ -1,11 +1,9 @@
 const { Router } = require('express') ;
-const { hashPass, verifyUser } = require('../middleware');
-
+const { hashPass, decryptUser } = require('../middleware');
 const { addUser, listUsers, updateUser, deleteUser } = require('./userController')
-const { decryptUser } = require(`../middleware`)
 const userRouter = Router();
 
-userRouter.get('/login', decryptUser); // moved from login routes
+userRouter.get('/login', decryptUser ); // moved from login routes
 userRouter.post('/signup', hashPass, addUser)
 userRouter.get('/user', hashPass, listUsers)
 userRouter.patch('/resetpassword', hashPass, updateUser)
